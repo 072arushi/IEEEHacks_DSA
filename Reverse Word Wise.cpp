f8913bf80ca4926759cefceee988720805b88e12
Jainsuhani2021 <104892962+Jainsuhani2021@users.noreply.github.com>
2022-11-05 23:28:50 +0530

    Update Reverse Word Wise.cpp

--- Reverse Word Wise.cpp
+++ Reverse Word Wise.cpp
@@ -1,6 +1,6 @@
 /*Reverse the given string word wise. That is, the last word in given string should come at 1st place, last second word at 2nd place and so on. Individual words should remain as it is.*/
-/*Input:Welcome to Coding Ninjas
-  Output:Ninjas Coding to Welcome*/
+/*Input:hello everyone
+  Output:everyone hello*/
 //Code:
 #include <iostream>
 #include<cstring>
