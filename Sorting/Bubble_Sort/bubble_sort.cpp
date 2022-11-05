2595640ea6a0ff08eff16bdbd789085e236cc176
Drishti Jain <drishtijain2103@gmail.com>
2022-11-05 17:43:25 +0530

    added driver's code

--- Sorting/Bubble_Sort/bubble_sort.cpp
+++ Sorting/Bubble_Sort/bubble_sort.cpp
@@ -1,3 +1,6 @@
+#include<iostream>
+using namespace std;
+
 void bubbleSort(int *input, int n){
     for(int i=0;i<=n-2;i++){
         for(int j=0;j<=n-2-i;j++){
@@ -7,3 +10,31 @@ void bubbleSort(int *input, int n){
                 input[j]=input[j+1];
                 input[j+1]=temp;
             }
+            else{
+                continue;
+            }
+        }
+    }
+}
+
+int main(){
+    int size;
+    cout<<"Enter the size of the array";
+    cin>>size;
+    int input[size];
+    for(int i=0;i<size;i++){
+        cout<<"Enter the data value";
+        cin>>input[i];
+    }
+
+    // calling the sorting function
+    bubbleSort(input,size);
+
+    cout<<endl;
+    //printing the sorted array
+    for(int i=0;i<size;i++){
+        cout<<input[i]<<" ";
+    }
+
+    return 0;
+}
